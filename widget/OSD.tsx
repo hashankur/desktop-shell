28a6741d9820ba852355aafe315c14538845a7d1
Hashan Kurukulasuriya <aeonian.adroit24@gmail.com>
2024-10-22 23:19:43 +0530

    feat: add audio OSD

--- widget/OSD.tsx
+++ widget/OSD.tsx
@@ -1,6 +1,7 @@
-import { App, Astal, Gdk, Gtk } from "astal/gtk3";
+import { App, Astal, Gtk } from "astal/gtk3";
 import { bind, timeout } from "astal";
 import Brightness from "../lib/brightness";
+import Wp from "gi://AstalWp";
 
 const WINDOW_NAME = "osd";
 
@@ -40,6 +41,63 @@ function BrightnessSlider() {
   );
 }
 
+function VolumeSlider() {
+  const audio = Wp.get_default().audio;
+
+  const icons = {
+    101: "overamplified",
+    67: "high",
+    34: "medium",
+    1: "low",
+    0: "muted",
+  };
+
+  function getIcon(vol) {
+    const icon =
+      vol === 0
+        ? 0
+        : [101, 67, 34, 1, 0].find(
+            (threshold) => threshold <= audio.defaultSpeaker.volume * 100,
+          );
+
+    return `audio-volume-${icons[icon]}-symbolic`;
+  }
+
+  return (
+    <revealer
+      transitionType={Gtk.RevealerTransitionType.SLIDE_LEFT}
+      transitionDuration={125}
+      setup={(self) => {
+        let i = 0;
+
+        self.hook(bind(audio.defaultSpeaker, "volume"), () => {
+          self.set_reveal_child(true);
+          i++;
+          timeout(1000, () => {
+            i--;
+
+            if (i === 0) self.set_reveal_child(false);
+          });
+        });
+      }}
+    >
+      <box className="Osd-Base" vertical>
+        <slider
+          className="Osd-Slider"
+          vertical
+          value={bind(audio.defaultSpeaker, "volume")}
+          onDragged={({ value }) => (audio.defaultSpeaker.volume = value)}
+          drawValue={false}
+          inverted
+        />
+        <icon
+          icon={bind(audio.defaultSpeaker, "volume").as((vol) => getIcon(vol))}
+        />
+      </box>
+    </revealer>
+  );
+}
+
 export default function OSD() {
   return (
     <window
@@ -50,8 +108,9 @@ export default function OSD() {
       layer={Astal.Layer.OVERLAY}
       anchor={Astal.WindowAnchor.RIGHT}
     >
-      <box vertical>
+      <box>
         <BrightnessSlider />
+        <VolumeSlider />
       </box>
     </window>
   );
