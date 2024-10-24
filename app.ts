07d74c49d1409fdc1573a2f427d26d9befbecfbb
Hashan Kurukulasuriya <aeonian.adroit24@gmail.com>
2024-10-24 18:42:17 +0530

    feat: add clipboard picker

--- app.ts
+++ app.ts
@@ -3,6 +3,7 @@ import { App } from "astal/gtk3";
 import { monitorFile } from "astal/file";
 import AppLauncher from "./widget/AppLauncher";
 // import Bar from "./widget/Bar"
+import Clipboard from "./widget/Clipboard";
 import Media from "./widget/Media";
 import OSD from "./widget/OSD";
 
@@ -12,6 +13,7 @@ App.start({
     AppLauncher();
     // Bar(0)
     // Bar(1) // initialize other monitors
+    Clipboard();
     Media();
     OSD();
   },
---
+++ widget/Clipboard.tsx
@@ -0,0 +1,99 @@
+import { App, Astal, Gdk, Gtk, Widget } from "astal/gtk3";
+import { bind, Variable } from "astal";
+import { exec, execAsync, subprocess } from "astal/process";
+
+const WINDOW_NAME = "clipboard";
+
+const cliphist = Variable<string>("");
+const query = Variable<string>("");
+
+export default function Clipboard() {
+  const Entry = new Widget.Entry({
+    text: bind(query),
+    hexpand: true,
+    canFocus: true,
+    placeholderText: "Search",
+    className: "AppLauncher-Input",
+    // primaryIconName: "edit-find",
+    onActivate: () => {
+      // items.get()[0]?.app.launch(); // TODO: fix launch first item on press enter
+      App.toggle_window(WINDOW_NAME);
+    },
+    setup: (self) => {
+      self.hook(self, "notify::text", () => {
+        query.set(self.get_text());
+      });
+    },
+  });
+
+  return (
+    <window
+      name={WINDOW_NAME}
+      application={App}
+      visible={false}
+      keymode={Astal.Keymode.EXCLUSIVE}
+      exclusivity={Astal.Exclusivity.NORMAL}
+      layer={Astal.Layer.OVERLAY}
+      vexpand={true}
+      onKeyPressEvent={(self, event) => {
+        if (event.get_keyval()[1] === Gdk.KEY_Escape) {
+          if (self.visible) {
+            query.set("");
+            Entry.grab_focus();
+            self.visible = false;
+          }
+        }
+      }}
+      setup={(self) => {
+        self.hook(self, "notify::visible", () => {
+          if (!self.get_visible()) {
+            query.set("");
+            // TODO: reset scroll
+          } else {
+            cliphist.set(exec("cliphist list"));
+            // Entry.grab_focus();
+          }
+        });
+      }}
+    >
+      <box className="AppLauncher base" vertical>
+        {/* {Entry} */}
+        <scrollable vexpand>
+          <box vertical spacing={10}>
+            {bind(cliphist).as((str) =>
+              str.split("\n").map((item) => (
+                <button
+                  className="Clipboard-Item"
+                  on_Clicked={() => {
+                    execAsync([
+                      "sh",
+                      "-c",
+                      `cliphist decode ${(item.match("[0-9]+") ?? [""])[0]} | wl-copy`,
+                    ]);
+                    App.toggle_window(WINDOW_NAME);
+                  }}
+                >
+                  {/* {item.match("binary.*(jpg|jpeg|png|bmp)") ? (
+                  <box
+                    className="Cover"
+                    valign={Gtk.Align.CENTER}
+                    css={`
+                      background-image: url("data:image/png;base64,${execAsync([
+                        "sh",
+                        "-c",
+                        `cliphist decode ${(item.match("[0-9]+") ?? [""])[0]} | base64 -w 0`,
+                      ])}");
+                    `}
+                  />
+                ) : ( */}
+                  <label label={item} truncate halign={Gtk.Align.START} />
+                  {/* )} */}
+                </button>
+              )),
+            )}
+          </box>
+        </scrollable>
+      </box>
+    </window>
+  );
+}
