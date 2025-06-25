ee2961c0d4102bc574cf903e2cfb522d12eaafa8
Hashan Kurukulasuriya <aeonian.adroit24@gmail.com>
2025-06-25 13:05:41 +0530

    refactor: media to ags3

--- widget/Media.tsx
+++ widget/Media.tsx
@@ -1,9 +1,9 @@
-import Mpris from "gi://AstalMpris";
-import Pango from "gi://Pango";
 import Window from "@/common/window";
 import icons from "@/util/icons";
-import { bind } from "astal";
-import { Astal, Gtk } from "astal/gtk4";
+import { createBinding } from "ags";
+import { Astal, Gtk } from "ags/gtk4";
+import Mpris from "gi://AstalMpris";
+import Pango from "gi://Pango";
 
 const WINDOW_NAME = "media";
 
@@ -13,87 +13,77 @@ export default function Media() {
 
     return (
       <>
-        {bind(spotify, "available").as((available) =>
-          available ? (
-            <box cssClasses={["min-w-[800px]"]}>
-              <image
-                file={bind(spotify, "coverArt")}
-                cssClasses={["min-h-[350px]", "min-w-[350px]", "rounded-lg"]}
-                overflow={Gtk.Overflow.HIDDEN}
-              />
-              <box vertical hexpand cssClasses={["p-10"]}>
-                <label
-                  label={bind(spotify, "title")}
-                  cssClasses={[
-                    "font-black",
-                    "mb-3",
-                    "text-on_surface",
-                    "text-3xl",
-                  ]}
-                  xalign={0} // Align label left
-                  wrap
-                  maxWidthChars={10} // How work ???
-                />
-                <label
-                  label={bind(spotify, "artist")}
-                  cssClasses={[
-                    "text-xl",
-                    "mb-2",
-                    "font-medium",
-                    "text-on_surface_variant",
-                  ]}
-                  xalign={0}
-                  ellipsize={Pango.EllipsizeMode.END}
-                />
-                <label
-                  label={bind(spotify, "album")}
-                  cssClasses={[
-                    "text-lg",
-                    "font-medium",
-                    "text-on_surface_variant",
-                  ]}
-                  xalign={0}
-                  ellipsize={Pango.EllipsizeMode.END}
-                  maxWidthChars={10} // How work ???
-                />
-                <box valign={Gtk.Align.END} halign={Gtk.Align.START} vexpand>
-                  {/*
+        <box
+          class="min-w-[800px]"
+          visible={createBinding(spotify, "available")}
+        >
+          <image
+            file={createBinding(spotify, "coverArt")}
+            class="min-h-[350px] min-w-[350px] rounded-lg"
+            overflow={Gtk.Overflow.HIDDEN}
+          />
+          <box orientation={Gtk.Orientation.VERTICAL} hexpand class="p-10">
+            <label
+              label={createBinding(spotify, "title")}
+              class="font-black mb-3 text-on_surface text-3xl"
+              xalign={0} // Align label left
+              wrap
+              maxWidthChars={10} // How work ???
+            />
+            <label
+              label={createBinding(spotify, "artist")}
+              class="text-xl mb-2 font-medium text-on_surface_variant"
+              xalign={0}
+              ellipsize={Pango.EllipsizeMode.END}
+            />
+            <label
+              label={createBinding(spotify, "album")}
+              class="text-lg font-medium text-on_surface_variant"
+              xalign={0}
+              ellipsize={Pango.EllipsizeMode.END}
+              maxWidthChars={10} // How work ???
+            />
+            <box valign={Gtk.Align.END} halign={Gtk.Align.START} vexpand>
+              {/*
                   <circularprogress
                     startAt={0.75}
                     endAt={0.75}
-                    value={bind(spotify, "position").as(
+                    value={createBinding(spotify, "position").as(
                       (position) => position / spotify.length,
                     )}
                     rounded
                     className="Progress"
                   >
                   */}
-                  <button onClicked={() => spotify.play_pause()}>
-                    <image
-                      iconName={bind(spotify, "playbackStatus").as((status) =>
-                        status === Mpris.PlaybackStatus.PLAYING
-                          ? icons.media.playing
-                          : icons.media.stopped,
-                      )}
-                    />
-                  </button>
-                  {/*
+              <button onClicked={() => spotify.play_pause()}>
+                <image
+                  iconName={createBinding(spotify, "playbackStatus").as(
+                    (status) =>
+                      status === Mpris.PlaybackStatus.PLAYING
+                        ? icons.media.playing
+                        : icons.media.stopped,
+                  )}
+                />
+              </button>
+              {/*
                     </circularprogress>
                   */}
-                </box>
-              </box>
             </box>
-          ) : (
-            <label label="No Media Playing" cssClasses={["text-2xl", "p-5"]} />
-          ),
-        )}
+          </box>
+        </box>
+
+        <label
+          label="No Media Playing"
+          class="text-2xl p-5"
+          visible={!createBinding(spotify, "available")}
+        />
       </>
     );
   };
 
   return (
     <Window name={WINDOW_NAME} anchor={Astal.WindowAnchor.BOTTOM}>
-      <box cssClasses={["p-1", "bg-surface_container_lowest", "rounded-xl"]}>
+      <box class="p-1 bg-surface_container_lowest rounded-xl">
         <SpotifyInfo />
       </box>
     </Window>
