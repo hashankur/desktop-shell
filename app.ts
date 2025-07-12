e03dd9aead5cdaaba9aca74d8a3fff7a0cd64a25
Hashan Kurukulasuriya <aeonian.adroit24@gmail.com>
2025-07-12 23:51:19 +0530

    chore: remove applauncher autostart

--- app.ts
+++ app.ts
@@ -1,19 +1,14 @@
 import AppLauncher from "@/widget/AppLauncher";
 import Bar from "@/widget/Bar";
-import Clipboard from "@/widget/Clipboard";
 // import Desktop from "@/widget/Desktop";
+import { notifyLowBattery } from "@/lib/battery";
 import Media from "@/widget/Media";
+import NotificationPopups from "@/widget/notifications/NotificationPopups";
 import OSD from "@/widget/OSD";
 import PowerMenu from "@/widget/PowerMenu";
 import SideBar from "@/widget/sidebar";
-import NotificationPopups from "@/widget/notifications/NotificationPopups";
 import app from "ags/gtk4/app";
 import { exec } from "ags/process";
-import type { Gtk } from "ags/gtk4";
-import GLib from "gi://GLib";
-import { notifyLowBattery } from "@/lib/battery";
-
-let applauncher: Gtk.Window;
 
 // https://github.com/Aiz0/dotless
 const style = exec("bunx tailwindcss -i styles/main.css")
@@ -22,23 +17,8 @@ const style = exec("bunx tailwindcss -i styles/main.css")
 
 app.start({
   css: style,
-  requestHandler(request, res) {
-    const [, argv] = GLib.shell_parse_argv(request);
-    if (!argv) return res("argv parse error");
-
-    switch (argv[0]) {
-      case "toggle":
-        applauncher.visible = !applauncher.visible;
-        return res("ok");
-      default:
-        return res("unknown command");
-    }
-  },
   main() {
-    applauncher = AppLauncher() as Gtk.Window;
-    app.add_window(applauncher);
-    applauncher.present();
-    // AppLauncher();
+    AppLauncher();
     Bar(0);
     // Bar(1) // initialize other monitors
     // Clipboard();
